721175ff80f194ca6714f1093de4903f92083dd8
Mohtadi Mahim <mohtadi.mahim@stonybrook.edu>
2024-08-17 02:03:12 -0400

    added few things up to 25:40 of video

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,20 +1,40 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+import Stripe from "stripe";
 
 const systemPrompt = `
   You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
   Both front and back should be one sentence long.
   You should return in the following JSON format:
   {
-  "flashcards":[
-    {
-      "front": "Front of the card",
-      "back": "Back of the card"
+    "flashcards":[
+      {
+        "front": "Front of the card",
+        "back": "Back of the card"
       }
-  ]
+    ]
   }
 `
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-11-15',
+})
+
+export async function POST(req) {
+  const openai = OpenAI()
+  const data = await req.text()
+
+  const completion = await openai.chat.completion.create({
+    messages: [
+      { role: 'system', content: systemPrompt },
+      { role: 'user', content: data }
+    ],
+    model: "gpt-4o",
+    response_format: { type: 'json_object' }
+  })
+}
+
+
 export async function generate(req) {
-    
+
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,38 @@
+import getStripe from "@/utils/get-stripe"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material"
+import Head from "next/head"
+
+
 
 export default function Home() {
   return (
-    <>
-      Hi
-    </>
+    <Container maxWidth="100vw">
+      <Head>
+        <title>Flashcard SaaS</title>
+        <meta name="description" content="Create flashcards from your text" />
+      </Head>
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+
+      <Box>
+        <Typography variant="h2">Welcome to Flashcard SaaS</Typography>
+      </Box>
+
+
+    </Container>
   )
 }
